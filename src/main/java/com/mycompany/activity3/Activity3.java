bbd1b56e06ceb8e625cd546bd63e94a5c2d011a1
MenardJakeP <MenardJakeP@192.168.0.126>
2024-02-14 14:40:45 +0800

    Activity 3

---
+++ src/main/java/com/mycompany/activity3/Activity3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.activity3;
+
+/**
+ *
+ * @author MenardJakeP
+ */
+public class Activity3 {
+
+    public static void main(String[] args) {
+        double c = 21f;
+        double a =1.7f;
+        double b = 34f;
+        
+        double result = c * a + b;
+         
+        System.out.println("fahrenhiet:"+ c * a + b);
+    }
+}
